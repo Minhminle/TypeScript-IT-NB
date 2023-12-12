ea2a55b5c76cfe83f153ec3da51c3e27d1e2b493
Minhminle <voleminhnghia176202@gmail.com>
2023-12-12 18:43:59 +0700

    (Container Asian) Fix card -food.map

--- build_web_figma/src/components/Container/Container_  Asian.tsx
+++ build_web_figma/src/components/Container/Container_  Asian.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { Box, Card, Grid, Typography } from "@mui/material";
 
+const foods = [
+    {
+      id: 1,
+      imageSrc: "/images/1.png",
+      title: "Special Salad",
+      description: "Food is any substance consumed by an organism for nutritional support.",
+    },
+    {
+      id: 2,
+      imageSrc: "/images/2.png",
+      title: "Russian Salad",
+      description: "Food is any substance consumed by an organism for nutritional support.",
+    },
+    {
+      id: 3,
+      imageSrc: "/images/3.png",
+      title: "Ramen Bliss",
+      description: "Food is any substance consumed by an organism for nutritional support.",
+    },
+    {
+      id: 4,
+      imageSrc: "/images/4.png",
+      title: "American Salad",
+      description: "Food is any substance consumed by an organism for nutritional support.",
+    },
+  ];
+
 const Container_Asian: React.FC = () => {
   return (
     <Box sx={{backgroundColor:'#F8FBDC', color:'black', height:'900px'}}>
@@ -15,67 +42,27 @@ const Container_Asian: React.FC = () => {
       </Box>
 
       <Box mt={4} sx={{marginTop:'75px', marginLeft:'60px', textAlign:'center'}}>
-        <Grid container spacing={-3}>
-          {/* Hình ảnh 1 */}
-          <Grid item xs={6} sm={3}>
-          <Card sx={{ borderRadius: "30px", width: '291px', height: '495px'}}>
-            <img
-              src="/images/1.png"
-              alt="Asian Food 1"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <Typography variant="h6" sx={{position:'relative', top:'-30px', fontWeight: 'bolder'}}>Special Salad</Typography>
-            <Typography variant="body2" sx={{fontSize:'20px',position:'relative'}}>
-            Food is any substance consumed by an organism for nutritional support.        
-            </Typography>
-            </Card>
-          </Grid>
-
-          {/* Hình ảnh 2 */}
-          <Grid item xs={6} sm={3}>
+      <Grid container spacing={-3}>
+      {foods.map((food) => (
+        <Grid key={food.id} item xs={6} sm={3}>
           <Card sx={{ borderRadius: "30px", width: '291px', height: '495px' }}>
-            <img
-              src="/images/2.png"
-              alt="Asian Food 2"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <Typography variant="h6" sx={{position:'relative', top:'-30px', fontWeight: 'bolder'}}>Russian Salad</Typography>
-            <Typography variant="body2" sx={{fontSize:'20px',position:'relative'}}>
-            Food is any substance consumed by an organism for nutritional support.        
+            <div style={{height:'325px', marginTop:'10px'}}>
+                <img
+                src={food.imageSrc}
+                alt={food.title}
+                style={{ width: "100%", height: "auto" }}
+                />
+            </div>
+            <Typography variant="h6" sx={{ position: 'relative', top: '-10px', fontWeight: 'bolder' }}>
+              {food.title}
             </Typography>
-            </Card>
-          </Grid>
-
-          {/* Hình ảnh 3 */}
-          <Grid item xs={6} sm={3}>
-          <Card sx={{ borderRadius: "30px", width: '291px', height: '495px' }}>
-            <img
-              src="/images/3.png"
-              alt="Asian Salad"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <Typography variant="h6" sx={{position:'relative', fontWeight: 'bolder'}}>Ramen Bliss</Typography>
-            <Typography variant="body2" sx={{fontSize:'20px',position:'relative', top:'30px'}}>
-            Food is any substance consumed by an organism for nutritional support.        
+            <Typography variant="body2" sx={{ fontSize: '20px', position: 'relative' }}>
+              {food.description}
             </Typography>
-            </Card>
-          </Grid>
-
-          {/* Hình ảnh 4 */}
-          <Grid item xs={6} sm={3}>
-          <Card sx={{ borderRadius: "30px", width: '291px', height: '495px' }}>
-            <img
-              src="/images/4.png"
-              alt="Asian Food 4"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <Typography variant="h6" sx={{position:'relative', top:'10px', fontWeight: 'bolder'}}>American Salad</Typography>
-            <Typography variant="body2" sx={{fontSize:'20px',position:'relative', top:'40px'}}>
-            Food is any substance consumed by an organism for nutritional support.        
-            </Typography>
-            </Card>
-          </Grid>
+          </Card>
         </Grid>
+      ))}
+    </Grid>
       </Box>
     </Box>
   );
