2a14de54f5d92b4f9df45805c6bf8a223f6f24d4
Minhminle <voleminhnghia176202@gmail.com>
2023-12-21 19:18:42 +0700

    add Layout use Layout for index. Homework: use Layout (switch, button, chuyển page...)

--- build_web_figma/src/components/Container/Container_Customer.tsx
+++ build_web_figma/src/components/Container/Container_Customer.tsx
@@ -1,49 +1,44 @@
 import React from "react";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Image from "next/image";
+import { Box, Typography, Card, Avatar, Stack } from "@mui/material";
 
-const Container_Cus = () => {
+const ContainerComponent = () => {
   return (
-    <Stack>
-      <Box textAlign={"center"} sx={{ width: "50%", paddingTop: "200px" }}>
-        <Typography
-          variant="h3"
-          sx={{ fontWeight: "bold", width: "80%", margin: "auto" }}
-        >
+    <Box width="100%" sx={{ backgroundColor: "rgba(218, 233, 82, 0.20);" }}>
+      {/* Tiêu đề và mô tả */}
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom>
           Our Dhabi Restaurant Happy Customers
         </Typography>
-        <Typography
-          variant="body1"
-          paragraph
-          sx={{
-            width: "50%",
-            margin: "auto",
-            fontSize: "20px",
-            paddingTop: "20px",
-          }}
-        >
+        <Typography variant="body1" sx={{ width: "70%", margin: "auto" }}>
           A customer is a person or business that buys goods or services from
           another business. Customers are crucial because they generate revenue.
           Without them, businesses would go out of business.
         </Typography>
       </Box>
-      <Box sx={{ width: "50%" }}>
+      {/* Stack chứa hình ảnh và mô tả */}
+      <Stack direction="row" spacing={2} alignItems="center">
+        {/* Hình ảnh */}
         <img
           src="/images/6.png"
           alt="Description"
           style={{
             width: "70%",
             height: "auto",
-            position: "relative",
-            zIndex: "1",
-            left: "110px ",
           }}
         />
-      </Box>
-    </Stack>
+        {/* Card chứa mô tả */}
+        <Card>
+          <Avatar src="/images/avacus.png" alt="ava customer" />
+          <Typography variant="body2">
+            A customer is a person or business that buys goods or services from
+            another business. Customers are crucial because they generate
+            revenue. Without them, businesses would go out of business.
+          </Typography>
+          <Typography variant="h6">Abdullah Iqbal</Typography>
+        </Card>
+      </Stack>
+    </Box>
   );
 };
 
-export default Container_Cus;
+export default ContainerComponent;
