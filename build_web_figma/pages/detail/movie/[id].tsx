c6b5dc3db0574bcf0d7d1618b2757c8657f1599b
Minhminle <voleminhnghia176202@gmail.com>
2024-01-04 19:34:52 +0700

    add credits ... update detail

--- build_web_figma/pages/detail/movie/[id].tsx
+++ build_web_figma/pages/detail/movie/[id].tsx
@@ -2,10 +2,40 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import axios from "axios";
-import { Movie } from "@/models/Movie";
 import Layout from "@/src/components/layoutDetailMovie";
-import { Box, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { ReactElement } from "react";
+import { Movie as BaseMovie } from "@/models/Movie";
+import { Rating, Chip } from "@mui/material";
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string;
+  // Thêm các thuộc tính khác của cast nếu cần
+}
+
+export interface Movie extends BaseMovie {
+  vote_average?: number;
+  vote_count?: number;
+  genres?: Genre[];
+  credits?: {
+    cast?: Cast[];
+  };
+}
 
 const MovieDetail = () => {
   const router = useRouter();
@@ -13,11 +43,13 @@ const MovieDetail = () => {
 
   const fetcher = (url: string) =>
     axios.get(url).then((response) => response.data);
-  const { data, error } = useSWR<Movie>(`/movie/${id}`, fetcher);
+  const { data, error } = useSWR<Movie>(
+    `/movie/${id}?append_to_response=credits`,
+    fetcher
+  );
 
   if (error) return <div>Error loading movie details</div>;
   if (!data) return <div>Loading...</div>;
-
   return (
     <Stack gap={4} direction={"row"}>
       <Box
@@ -47,6 +79,53 @@ const MovieDetail = () => {
           sx={{ fontSize: "18px", color: "#555", marginBottom: "20px" }}
         >
           {data.overview}
+        </Typography>
+        <Stack direction={"row"} spacing={"20px"}>
+          <Rating
+            name="movie-rating"
+            value={data.vote_average ? data.vote_average / 2 : 0}
+            precision={0.5}
+            readOnly
+            sx={{ color: "#FFD700", fontSize: 24 }}
+          />
+          <Typography
+            variant="h5"
+            sx={{ color: "#d32f2f", marginBottom: "10px" }}
+          >
+            Votes: {data.vote_count}
+          </Typography>
+        </Stack>
+        <Typography variant="h5" sx={{ color: "#42a5f5", marginTop: "10px" }}>
+          Genres:
+          {data.genres?.map((genre) => (
+            <Chip
+              key={genre.id}
+              label={genre.name}
+              sx={{
+                margin: "3px",
+                backgroundColor: "#673ab7",
+                color: "white",
+                marginRight: "5px",
+                marginBottom: "5px",
+              }}
+            />
+          ))}
+        </Typography>
+        <Typography variant="h5" sx={{ color: "#1de9b6", marginTop: "10px" }}>
+          Cast:
+          {data.credits?.cast?.slice(0, 10).map((actor) => (
+            <Chip
+              key={actor.id}
+              label={`${actor.name} as ${actor.character}`}
+              sx={{
+                margin: "3px",
+                backgroundColor: "#66bb6a",
+                color: "white",
+                marginRight: "5px",
+                marginBottom: "5px",
+              }}
+            />
+          ))}
         </Typography>
       </Box>
     </Stack>
